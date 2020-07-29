d9a1ed2056348001b63da0e2233bee55bdfc8d15
Abdullah Khan <abdullahkhan.sse@outlook.com>
2020-07-29 22:44:17 +0530

    School Management System

---
+++ SchoolManagement/SchoolManagement.Tests/Controllers/StudentControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using SchoolManagement.Controllers;
+using SchoolManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SchoolManagement.Tests.Controllers
+{
+    [TestFixture]
+    public class StudentControllerTest
+    {
+        [Test]
+        public void TestStudentIndex()
+        {
+
+            //var mock = new Mock<IDbContext>();
+
+            //mock.Setup(x => x.Set<Student>())
+            //.Returns(new FakeDbSet<Student>
+            //{
+            //    new Student{ID=567567, FirstMidName="Abdullah", LastName="Khan", EnrollmentDate=new DateTime()}
+            //});
+
+            var obj = new StudentsController();
+
+            var actResult = obj.Index() as ViewResult;
+
+            Assert.That(actResult.ViewName, Is.EqualTo("Index"));
+        }
+    }
+}
---
+++ SchoolManagement/SchoolManagement.Tests/Fakes/IDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Tests.Fakes
+{
+    public interface IDbContext : IDisposable
+    {
+        IDbSet<T> Set<T>() where T : class;
+        DbEntityEntry<T> Entry<T>(T entity) where T : class;
+        int SaveChanges();
+    }
+}
