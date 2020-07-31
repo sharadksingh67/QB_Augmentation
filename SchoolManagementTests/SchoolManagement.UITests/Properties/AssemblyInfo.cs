e9ea1f1ff2fb449f6a9c627937293f9376b81548
Abdullah Khan <abdullahkhan.sse@outlook.com>
2020-07-31 13:50:51 +0530

    Test Cases

---
+++ SchoolManagementTests/SchoolManagement.UITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolManagement.UITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("SchoolManagement.UITests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18e11ecf-29e6-47c9-b183-e19afb1268e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchoolManagementTests/SchoolManagement.UITests/SchoolManagementSystemTests.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchoolManagement.UITests
+{
+    public class SchoolManagementSystemTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public SchoolManagementSystemTests()
+        {
+            _driver = new ChromeDriver(@"D:\Assignments\Cognizant\ICT\Now\SchoolManagementTests\SchoolManagement.UITests\bin\Debug");
+        }
+
+        [Fact]
+        public void Test_Index()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44316/Student/index");
+
+            Assert.Equal("Index - Student Management Application", _driver.Title);
+            var links = _driver.FindElements(By.TagName("a"));
+
+
+            Assert.Equal("School Management System", links[0].Text);
+
+            Assert.Equal("Home", links[1].Text);
+
+            Assert.Equal("Create New", links[2].Text);
+
+        }
+
+        [Fact]
+        public void Test_Details()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44316/Students/Details/1");
+
+            Assert.Equal("Details - Restaurant Management Application", _driver.Title);
+            var links =
+                _driver.FindElements(By.TagName("a"));
+
+            Assert.Equal("Back to List", links[2].Text);
+        }
+
+        [Fact]
+        public void Test_AddStudent_RequiredFields()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44316/Students/Create");
+
+            _driver.FindElement(By.Id("Create")).Click();
+
+            IWebElement nameErrorMessage =
+                _driver.FindElement(By.Id("LastName-error"));
+
+            Assert.Equal("The Last Name field is required.", nameErrorMessage.Text);
+        }
+
+        [Fact]
+        public void Test_AddStudent_RequiredFields_FirstName()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:44316/Students/Create");
+
+            _driver.FindElement(By.Id("Create")).Click();
+
+            IWebElement nameErrorMessage =
+                _driver.FindElement(By.Id("FirstMidName-error"));
+
+            Assert.Equal("The First Name field is required.", nameErrorMessage.Text);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
